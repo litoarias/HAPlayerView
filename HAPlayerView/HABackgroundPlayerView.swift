66a554744070d28160e7667885435ba997d21e24
Hipolito Arias <iecisa.drodrigue@iberia.es>
2017-02-14 11:39:55 +0100

    Initial commit

---
+++ HAPlayerView/HABackgroundPlayerView.swift
@@ -0,0 +1,108 @@
+
+import UIKit
+import AVFoundation
+
+@IBDesignable class HABackgroundPlayerView: UIView {
+    
+    var player: AVPlayer?
+    var avPlayerLayer: AVPlayerLayer!
+    var videoURL: NSURL = NSURL()
+    var cView: UIView = UIView()
+    
+    @IBInspectable
+    public var name: String = "test" {
+        didSet {
+            inits(resource: name, extensionFormat: mime, repeats: repeats, muted: muted, alpha: layerAlpha)
+        }
+    }
+    
+    @IBInspectable
+    public var mime: String = "mp4" {
+        didSet {
+            inits(resource: name, extensionFormat: mime, repeats: repeats, muted: muted, alpha: layerAlpha)
+        }
+    }
+    
+    @IBInspectable
+    public var repeats: Bool = true {
+        didSet {
+            inits(resource: name, extensionFormat: mime, repeats: repeats, muted: muted, alpha: layerAlpha)
+        }
+    }
+    
+    @IBInspectable
+    public var muted: Bool = true {
+        didSet {
+            inits(resource: name, extensionFormat: mime, repeats: repeats, muted: muted, alpha: layerAlpha)
+        }
+    }
+    
+    @IBInspectable
+    public var layerAlpha: CGFloat = 0.5 {
+        didSet {
+            inits(resource: name, extensionFormat: mime, repeats: repeats, muted: muted, alpha: layerAlpha)
+        }
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        inits(resource: name, extensionFormat: mime, repeats: repeats, muted: muted, alpha: layerAlpha)
+    }
+    
+    required public init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        inits(resource: name, extensionFormat: mime, repeats: repeats, muted: muted, alpha: layerAlpha)
+    }
+    
+    func inits(resource: String, extensionFormat: String, repeats: Bool, muted: Bool, alpha: CGFloat) {
+        
+        setLayer()
+        
+        guard let video = Bundle.main.url(forResource: resource,
+                                          withExtension: extensionFormat) else {
+            return
+        }
+        videoURL = video as NSURL
+        
+        if (player == nil) {
+            player = AVPlayer(url: videoURL as URL)
+            player?.actionAtItemEnd = .none
+            player?.isMuted = muted
+            
+            avPlayerLayer = AVPlayerLayer(player: player)
+            avPlayerLayer.videoGravity = AVLayerVideoGravityResizeAspectFill
+            avPlayerLayer.zPosition = -1
+            avPlayerLayer.frame = self.frame
+            
+            player?.play()
+            
+            layer.addSublayer(avPlayerLayer)
+
+        }
+        
+        if repeats {
+            NotificationCenter.default.addObserver(self,
+                                                   selector: #selector(loopVideo),
+                                                   name: NSNotification.Name.AVPlayerItemDidPlayToEndTime,
+                                                   object: player?.currentItem)
+        }
+        
+    }
+    
+    func loopVideo() {
+        player?.seek(to: kCMTimeZero)
+        player?.play()
+    }
+    
+    func setLayer() {
+        if cView.isDescendant(of: self) {
+            cView.removeFromSuperview()
+        } else {
+            cView = UIView.init(frame: self.frame)
+            cView.backgroundColor = UIColor.black
+            cView.alpha = layerAlpha;
+            cView.layer.zPosition = 0;
+            self.addSubview(cView)
+        }
+    }
+}
---
+++ HAPlayerView/HAPlayerView.h
@@ -0,0 +1,19 @@
+//
+//  HAPlayerView.h
+//  HAPlayerView
+//
+//  Created by Hipolito Arias on 14/2/17.
+//  Copyright © 2017 Hipolito Arias. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HAPlayerView.
+FOUNDATION_EXPORT double HAPlayerViewVersionNumber;
+
+//! Project version string for HAPlayerView.
+FOUNDATION_EXPORT const unsigned char HAPlayerViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HAPlayerView/PublicHeader.h>
+
+
